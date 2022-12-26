2b2fbfbb84c49cb92507f4be6681f61c1b3042b7
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2022-12-26 20:41:03 +0530

    Java

---
+++ KNGTOR.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class KNGTOR{
+	public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0){
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    System.out.println((n*5)+(m*7));
+		}
+	}
+}

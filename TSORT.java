e1e4f48a105314b3cf9d6492628e0fee75c3d593
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2023-01-06 22:56:13 +0530

    Java

---
+++ TSORT.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Arrays;
+class TSORT{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a[] = new int[t];
+        for(int i=0;i<t;i++){
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        for(int j=0;j<a.length;j++){
+            System.out.println(a[j]);
+        }
+    }
+}
